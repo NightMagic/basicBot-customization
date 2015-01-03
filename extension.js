0a5baa83e3ef7a506bc82d41bb605020d007771f
Exuviax <exuviax@gmail.com>
2015-01-03 13:42:00 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -52,7 +52,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Go check out Skyasi's stream! www.twitch.tv/exuviax");
+                    API.sendChat("/me Go check out Skyasi's stream! http://www.twitch.tv/skyasi");
                 }
             }
         };
