20b3b438913ea428fe61b32d2efc379cba18a459
Exuviax <exuviax@gmail.com>
2015-01-03 00:40:50 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -68,6 +68,18 @@
                 }
             }
         };
+        bot.commands.subCommand = {
+            command: 'TAS',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me If you purchase a subscription to Skyasi on twitch you will receive Resident DJ in chat, if you are already a Sub please let a manager know and we will add you. You will need to confirm in the twitch chat that you are a sub, Resident is more of a superficial role and less of an actual power role.");
+                }
+            }
+        };
         bot.commands.exuCommand = {
             command: 'exu',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
