8e8e91d2c690ca82048e8e996d30eba3c7258acc
Exuviax <exuviax@gmail.com>
2015-01-03 22:47:29 -0500

    Switched to local Variable

--- extension.js
+++ extension.js
@@ -165,6 +165,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript('https://rawgit.com/benzi/basicBot/master/basicBot.js', extend);
+    $.getScript('https://rawgit.com/Exuviax/basicBot/master/basicBot.js', extend);
 
 }).call(this);
